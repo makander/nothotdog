0ee43408b12e05f93af11116a80aeb6dc01d2a4f
nils <nils@Nils-MBP16.lan>
2021-02-26 13:06:03 +0100

    can log in user at login, redirects to home

--- nothotdog/urls.py
+++ nothotdog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('home/', views.home, name="home"),
+    path('login/', views.my_view, name="login")
 ]
