47182d30f2c5f208feaff6881fab1a5468f7d271
nils <nils@Nils-MBP16.lan>
2021-03-05 14:10:18 +0100

    change auth classes in settings

--- myproject/nothotdog/urls.py
+++ myproject/nothotdog/urls.py
@@ -12,7 +12,7 @@ router.register(r'images', views.ImageListView),
 urlpatterns = [
     path('', views.Index.as_view(), name="index"),
     path('home/', login_required(views.Home.as_view()), name="home"),
-    path('login/', auth_views.LoginView.as_view(template_name="login.html", redirect_authenticated_user=True), name="login"),
+    path('login/', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name="logout.html"), name="logout"),
     path('image_form/', views.ImageCreate.as_view()),
     path('image/<uuid:pk>/', views.ImageDetail.as_view(), name="image"),
@@ -20,6 +20,7 @@ urlpatterns = [
     path('images/', views.ImageList.as_view(), name="images"),
     path('my_images/', views.MyImages.as_view(), name="myimages"),
     #path('api/images', views.ImageListView.as_view({'get': 'list'}))
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/auth/', include('dj_rest_auth.urls'))
 ]
 
--- myproject/urls.py
+++ myproject/urls.py
@@ -23,8 +23,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myproject.nothotdog.urls')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('dj-rest-auth/', include('dj_rest_auth.urls'))
+    #path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    #path('dj-rest-auth/', include('dj_rest_auth.urls'))
 ]
 
 
